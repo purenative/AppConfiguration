fc89c4904131e1303f2d6d861bfd0264e78924d4
Artem Eremeev <jeremy1002@yandex.ru>
2022-01-17 13:17:27 +0300

    Initial Commit

---
+++ Sources/AppConfiguration/AppConfiguration.swift
@@ -0,0 +1,6 @@
+public struct AppConfiguration {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/AppConfigurationTests/AppConfigurationTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AppConfiguration
+
+final class AppConfigurationTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AppConfiguration().text, "Hello, World!")
+    }
+}
